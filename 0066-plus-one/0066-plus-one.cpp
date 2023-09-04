83c43592df1712f98e19b780e9ad12c7751f4b55
Abhinav Mathur <91373992+Maniac1769@users.noreply.github.com>
2023-09-04 12:35:09 +0530

    Time: 0 ms (100%), Space: 8.8 MB (35.44%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
-    vector<int> plusOne(vector<int>& v) {
-        int n = v.size();
-        for(int i = n-1; i >= 0; i--){
-            if(i == n-1)
-                v[i]++;
-            if(v[i] == 10){
-                v[i] = 0;
-                if(i != 0){
-                    v[i-1]++;
-                }
-                else{
-                    v.push_back(0);
-                    v[i] = 1;
-                }
-            }
+    vector<int> plusOne(vector<int>& digits) {
+        vector<int> ans;
+        int n = digits.size();
+        int temp;
+        int carry = 1;
+        for(int i=n-1; i>=0; i--){
+            temp = digits[i]+carry;
+            carry = temp/10;
+            temp %= 10;
+            ans.push_back(temp);
         }
-        return v;
+        if(carry==1) ans.push_back(carry);
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
