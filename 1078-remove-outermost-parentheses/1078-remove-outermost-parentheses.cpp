8f9013b9f67374dc504e88e234aa560a16f2cfa8
Abhinav Mathur <91373992+Maniac1769@users.noreply.github.com>
2023-09-06 17:29:24 +0530

    Time: 5 ms (37.08%), Space: 6.7 MB (47.81%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -1,27 +1,17 @@
 class Solution {
-public:
-    string removeOuterParentheses(string s) {
-        string result;
-        stack <char> st;
-        for(int i:s)
-        {
-            if(i=='(')
-            {
-                if(!st.empty())
-                {
-                result.push_back(i);
-                }
-                st.push(i);
-            }
-            else{
-                st.pop();
-                if(!st.empty())
-                {
-                    result.push_back(i);
-                }
-            }
-            
-        }
-        return result;
+  public: string removeOuterParentheses(string s) {
+    string str;
+    int c = 0;
+    for (int i = 0; i < s.size(); i++) {
+      if (c == 0) {
+        c--;
+        continue;
+      }
+      if (s[i] == '(') c--;
+      else c++;
+      if (c == 0) continue;
+      str.push_back(s[i]);
     }
+    return str;
+  }
 };
