0d83b164ac34130f13454da3b013f3d9a113910d
Abhinav Mathur <91373992+Maniac1769@users.noreply.github.com>
2023-09-15 01:20:52 +0530

    Time: 6 ms (57.12%), Space: 10.3 MB (31.87%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
@@ -1,13 +1,31 @@
 class Solution {
 public:
-    bool checkIfExist(vector<int>& arr) {
-        unordered_map<int, int> mp;
-        for(int i=0; i<arr.size(); i++){
-            if(mp.find(arr[i]*2) != mp.end())
+    bool binarySearch (vector<int> &arr, int l, int r, int target)
+    {
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (arr[m] == target)
                 return true;
-            else if(arr[i]%2==0 && mp.find(arr[i]/2) != mp.end())
-                return true;
-            mp[arr[i]] = i;
+            else if (arr[m] > target)
+                r = m - 1;
+            else
+                l = m + 1;
+        }
+        return false;
+    }
+    
+    bool checkIfExist(vector<int>& arr) 
+    {
+        sort(arr.begin(), arr.end());
+        for(int i = 0; i < arr.size(); i++) {
+            if((arr[i] % 2 == 0) && arr[i] < 0) {
+                if(binarySearch(arr, i + 1, arr.size() - 1, arr[i] / 2))
+                    return true; 
+            }
+            else {
+                if (binarySearch(arr, i + 1, arr.size() - 1, 2 * arr[i]))
+                    return true;
+            }
         }
         return false;
     }
