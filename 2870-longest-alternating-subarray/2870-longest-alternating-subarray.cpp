a5a5ed647de75e8f83eba733db301047ce310852
Abhinav Mathur <91373992+Maniac1769@users.noreply.github.com>
2023-09-15 01:42:31 +0530

    Time: 8 ms (98.69%), Space: 65.4 MB (12.04%) - LeetHub

---
+++ 2870-longest-alternating-subarray/2870-longest-alternating-subarray.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int alternatingSubarray(vector<int>& nums) {
+    int j = 0, res = 0;
+    for (int i = 1; i < nums.size(); ++i) {
+        if (nums[i] != nums[j] + (i - j) % 2)
+            j = i - (nums[i - 1]  == nums[i] - 1);
+        res = max(res, i - j + 1);
+    }
+    return res == 1 ? -1 : res;
+}
+};
