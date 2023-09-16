bff38ba2e1ee6ca28382030ab6e883fc120f064b
Abhinav Mathur <91373992+Maniac1769@users.noreply.github.com>
2023-09-16 21:19:29 +0530

    Time: 123 ms (5.44%), Space: 41.4 MB (5.07%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.cpp
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int> set1(nums1.begin(), nums1.end());
-        unordered_set<int> set2(nums2.begin(), nums2.end());
+        int n1 = nums1.size(),n2 = nums2.size();
+        set<int> ans1, ans2;
+        int i = 0 ,j = 0;
+        vector<int>v1,v2;
         
-        vector<int> distinct_nums1, distinct_nums2;
-        for (int num : set1) {
-            if (set2.count(num) == 0) {
-                distinct_nums1.push_back(num);
-            }
+        unordered_map<int, int> m;
+        unordered_map<int, int> m1;
+        
+         while(i < n1 || j < n2) {
+            if( i < n1 ) m[nums1[i++]]++;    
+            if( j < n2 ) m1[nums2[j++]]++;
         }
 
-        for (int num : set2) {
-            if (set1.count(num) == 0) {
-                distinct_nums2.push_back(num);
-            }
+        i  = 0 ,j = 0;
+         while(i < n1 || j < n2) {
+            if( i < n2  && m[nums2[i]] == 0) ans2.insert (nums2[i]);    
+            if( j < n1 && m1[nums1[j]] == 0) ans1.insert(nums1[j]);
+            i++;
+            j++;
         }
 
-        return {distinct_nums1, distinct_nums2};
+
+        for(auto i : ans1) v1.push_back(i);
+        for(auto i : ans2) v2.push_back(i);
+
+        return {v1, v2};
+
     }
 };
