83566f73f9b4b75a7e5a8712ac7b7be1c662c2e6
Abhinav Mathur <91373992+Maniac1769@users.noreply.github.com>
2024-06-13 19:37:48 +0530

    Time: 8 ms (62.72%), Space: 8.2 MB (59.13%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,24 +1,43 @@
+// Companies :  Amazon Google
+// Sliding window (Two pointers)
+// Tc-O(n), SC-O(1)
+
 class Solution {
 public:
-     int characterReplacement(string s, int k) {
-        unordered_map<char, int> alphabets;
-        int ans = 0;
-        int left = 0;
-        int right = 0;
-        int maxf = 0;
+    int characterReplacement(string s, int k) {
+        int n = s.length();
+        int left = 0, right = 0;
+        
+        vector<int> freq(26, 0);  // stores char frequency
+        int maxFreq = 0;
+        int maxLen = 0;
 
-        for (right = 0; right < s.size(); right++) {
-            alphabets[s[right]] = 1 + alphabets[s[right]];
-            maxf = max(maxf, alphabets[s[right]]);
-
-            if ((right - left + 1) - maxf > k) {
-                alphabets[s[left]] -= 1;
+        while(right < n) {
+            freq[s[right] - 'A']++;  // increment frequency
+            maxFreq = max(maxFreq, freq[s[right] - 'A']);  // update maxFreq
+            
+            // when changes required is > k
+            if(( (right-left+1) - maxFreq ) > k){   // *** Remark , we use if instead of while as its more efficient
+                freq[s[left] - 'A']--;
+                maxFreq = 0;
+                for(int i=0; i<26; i++){
+                    maxFreq = max(maxFreq, freq[i]);
+                }
                 left++;
-            } else {
-                ans = max(ans, (right - left + 1));
             }
+
+            if( (right-left+1) - maxFreq  <=  k){
+                maxLen = max(maxLen, right-left+1);
+            }
+
+            right++;
         }
 
-        return ans;
+        return maxLen;
     }
 };
+
+/*
+*** Remark :
+the key difference in time complexity lies in how the maximum frequency is updated during the shrinking of the window. Approach 1 -> 'using while' has a quadratic time complexity(O(n^2)) due to recalculating the maximum frequency in each iteration of the while loop, while Approach 2 -> 'using if' has a linear time complexity(O(n)) since it updates the maximum frequency more efficiently. Approach 2 is more efficient in terms of time complexity.
+*/
