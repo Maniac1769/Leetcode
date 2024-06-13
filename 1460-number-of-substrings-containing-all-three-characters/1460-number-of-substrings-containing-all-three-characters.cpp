492305eacfe226e5b75e687ca1bc52b374179975
Abhinav Mathur <91373992+Maniac1769@users.noreply.github.com>
2024-06-13 18:56:40 +0530

    Time: 16 ms (67.21%), Space: 9.6 MB (65.65%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -6,9 +6,7 @@ public:
 
         for(int i = 0; i<s.size(); i++){
             lastSeen[s[i] - 'a'] = i;
-            if(lastSeen[0]!= -1 && lastSeen[1]!=-1 && lastSeen[2]!=-1){
-                cnt = cnt + (1 + min({lastSeen[0], lastSeen[1], lastSeen[2]}));
-            }
+            cnt = cnt + (1 + min({lastSeen[0], lastSeen[1], lastSeen[2]}));
         }
         return cnt;
     }
