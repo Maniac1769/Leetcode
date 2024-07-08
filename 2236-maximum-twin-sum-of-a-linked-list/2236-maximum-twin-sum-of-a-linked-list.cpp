59a99db97a236edaf5defc59326b5fc3bf07a6f7
Abhinav Mathur <91373992+Maniac1769@users.noreply.github.com>
2024-07-08 17:09:53 +0530

    Time: 168 ms (88.8%), Space: 119 MB (57.24%) - LeetHub

--- 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.cpp
+++ 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.cpp
@@ -1,31 +1,44 @@
 class Solution {
-    public:
-     int pairSum(ListNode* head) {
-        ListNode* slow = head;
-        ListNode* fast = head;
-        int maxVal = 0;
-
-        while(fast && fast -> next)
-        {
-            slow = slow -> next;
-            fast = fast -> next -> next;
+public:
+    ListNode* reverse(ListNode* head){
+        ListNode* r=NULL,*q=NULL,*first=head;
+        while(head){
+            r=q;
+            q=head;
+            head=head->next;
+            q->next=r;
         }
-
-        ListNode *nextNode, *prev = NULL;
-        while (slow) {
-            nextNode = slow->next;
-            slow->next = prev;
-            prev = slow;
-            slow = nextNode;
+        first=q;
+        return first;
+    }
+    
+    ListNode* findMiddleNode(ListNode* head){
+        ListNode* slowptr=head,*fastptr=head->next;
+        while(fastptr&&fastptr->next){
+            slowptr=slowptr->next;
+            fastptr=fastptr->next->next;
         }
-
-        while(prev)
-        {
-            maxVal = max(maxVal, head -> val + prev -> val);
-            prev = prev -> next;
-            head = head -> next;
+        return slowptr;
+    }
+    
+    int pairSum(ListNode* head) {
+        int ans=0;
+        
+        ListNode* midNode=findMiddleNode(head);
+        ListNode* head2=reverse(midNode->next);
+        
+        midNode->next=NULL;
+        
+        
+        ListNode* p1=head,*p2=head2;
+        while(p1&&p2){
+            ans=max(ans,p1->val+p2->val);
+            p1=p1->next;
+            p2=p2->next;
         }
-
-        return maxVal;
+        
+        midNode->next=reverse(head2);
+        
+        return ans;
     }
 };
