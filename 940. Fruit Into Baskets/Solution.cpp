d7c0a970baddd4d1c81c5f43d06306d8fe665005
Abhinav Mathur <91373992+Maniac1769@users.noreply.github.com>
2024-09-08 19:25:00 +0530

    C++ Solution
            Submission Statistics:
            Question #: 940
            Language: C++
            Runtime: 108 ms
            Runtime percentile: 87.60
            Memory: 74.816 MB
            Memory percentile: 88.26

--- 940. Fruit Into Baskets/Solution.cpp
+++ 940. Fruit Into Baskets/Solution.cpp
@@ -1,3 +1,29 @@
+// class Solution {
+// public:
+//     int totalFruit(vector<int>& fruits) 
+//     {
+//         int l=0,r=0,maxlen=0,len=0;
+//         int n=fruits.size();
+//         unordered_map <int,int> m;
+//         while(r<n)
+//         {
+//             m[fruits[r]]++;
+//             if(m.size()>2)
+//             {
+//                 m[fruits[l]]--;
+//                 if(m[fruits[l]]==0)
+//                     m.erase(fruits[l]);
+//                 l++;
+//             }
+//             if(m.size()<=2)
+//             maxlen=max(maxlen,r-l+1);
+//             r++;
+//         }
+//         return maxlen;
+//     }
+// };
+
+
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) 
@@ -8,7 +34,7 @@ public:
         while(r<n)
         {
             m[fruits[r]]++;
-            if(m.size()>2)
+            while(m.size()>2)
             {
                 m[fruits[l]]--;
                 if(m[fruits[l]]==0)
