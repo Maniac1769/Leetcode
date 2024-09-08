fc2611f344e5bfef5e9e7e149f044a10a49b5d27
Abhinav Mathur <91373992+Maniac1769@users.noreply.github.com>
2024-09-08 19:27:06 +0530

    C++ Solution
            Submission Statistics:
            Question #: 1046
            Language: C++
            Runtime: 43 ms
            Runtime percentile: 46.96
            Memory: 58.192 MB
            Memory percentile: 63.32

---
+++ 1046. Fruit Into Baskets/Solution.cpp
@@ -0,0 +1,54 @@
+//TWO WAYS FIRST IS STRIVER SOLUTION SECONF IS THE WAY ADITYA VERMA TAUGHT 
+
+// class Solution {
+// public:
+//     int totalFruit(vector<int>& fruits) 
+//     {
+//         int l=0,r=0,maxlen=0,len=0;
+//         int n=fruits.size();
+//         unordered_map <int,int> m;
+//         while(r<n)
+//         {
+//             m[fruits[r]]++;
+//             if(m.size()>2)
+//             {
+//                 m[fruits[l]]--;
+//                 if(m[fruits[l]]==0)
+//                     m.erase(fruits[l]);
+//                 l++;
+//             }
+//             if(m.size()<=2)
+//             maxlen=max(maxlen,r-l+1);
+//             r++;
+//         }
+//         return maxlen;
+//     }
+// };
+
+
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) 
+    {
+        int l=0,r=0,maxlen=0,len=0;
+        int n=fruits.size();
+        unordered_map <int,int> m;
+        while(r<n)
+        {
+            m[fruits[r]]++;
+            w
+            
+            hile(m.size()>2)
+            {
+                m[fruits[l]]--;
+                if(m[fruits[l]]==0)
+                    m.erase(fruits[l]);
+                l++;
+            }
+            if(m.size()<=2)
+            maxlen=max(maxlen,r-l+1);
+            r++;
+        }
+        return maxlen;
+    }
+};
