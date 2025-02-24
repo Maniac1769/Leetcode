5df8439fbcad8fbb9d2304f5416edc79f1daff99
Abhinav Mathur <91373992+Maniac1769@users.noreply.github.com>
2025-02-24 10:11:00 +0530

    Time: 0 ms (100.00%) | Memory: 64.9 MB (95.21%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.cpp
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    int findJudge(int n, vector<vector<int>> trust) {
-        if (trust.size() == 0 && n == 1) 
-            return 1;
-        vector<int> count(n + 1);
-        for (auto person : trust) {
-            //count[person[0]]--; decrements the trust count of the first person in the pair. This is done because if the first person trusts the second person, it means that the first person does not trust themselves.
-            count[person[0]]--;
-            //count[person[1]]++; increments the trust count of the second person in the pair. This is done because if the first person trusts the second person, it means that the second person is trusted by the first person.
-            count[person[1]]++;
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> trustCount(n + 1, 0);
+
+        for (auto& t : trust) {
+            trustCount[t[0]]--; // Decrease for those who trust someone
+            trustCount[t[1]]++; // Increase for those who are trusted
         }
 
-        for (int person = 0; person < count.size(); person++) {
-            // check if there is a person who is trusted by everyone except themselves (n-1 people) and if such person exists it returns the index of that person.
-            if (count[person] == n - 1) return person;
+        for (int i = 1; i <= n; i++) {
+            if (trustCount[i] == n - 1) return i; // Judge is trusted by everyone except themselves
         }
+
         return -1;
     }
 };
